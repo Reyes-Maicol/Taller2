08b253c9cb308397106c105b1883be4f18905fe1
Reyes-Maicol <reyesmaicol225@gmail.com>
2024-05-20 23:49:44 -0500

    Se termina la funcionalidad de registrar la empresa y la factura en la DB

--- Back/Controller/validarFormularios.js
+++ Back/Controller/validarFormularios.js
@@ -1,11 +1,35 @@
 const conn = require("../model/conn").promise();
 
-const validarFormularios = async (req,res)=>{
+const validarFormularios = async (req, res) => {
     try {
-        const {nomempresa,nitempresa,emailempresa,fecha,nompersona,cantproductos,productos}=req.body
-        console.log(req.body)
+        const { nomempresa, nitempresa, emailempresa, fecha, nompersona, cantproductos, productos } = req.body;
+        console.log(req.body);
+        console.log("Si llegaron");
+
+        const consulta = "SELECT * FROM empresa WHERE nombre_empresa=? AND nit=? AND correo=?";
+        const [verificar] = await conn.query(consulta, [nomempresa, nitempresa, emailempresa]);
+        console.log(verificar);
+
+        if (verificar.length > 0) {
+            const cons = "INSERT INTO factura (fecha_reporte, empresa_id, nombre_reportador, cantidad_productos, des_product) VALUES (?, ?, ?, ?, ?)";
+            const peticion = await conn.query(cons, [fecha, verificar[0].id_empresa, nompersona, cantproductos, JSON.stringify(productos)]);
+
+            res.send({ message: "Exitoso" });
+        } else {
+            const consultas ="INSERT INTO empresa (nombre_empresa,nit,correo) VALUES (?,?,?)"
+            const peticion = await conn.query(consultas,[nomempresa,nitempresa,emailempresa])
+            console.log(peticion)
+
+            if(peticion.affectedRows>0){
+                const insercionFactura ="INSERT INTO factura (fecha_reporte, empresa_id, nombre_reportador, cantidad_productos, des_product) VALUES (?, ?, ?, ?, ?)"
+                const pet = await conn.query(insercionFactura,[fecha, inssertId, nompersona, cantproductos, JSON.stringify(productos)])
+            }
+            res.status(200).send({ message: "Empresa registrada y factura registrada" });
+        }
     } catch (error) {
-        
+        console.error(error);
+        res.status(500).send({ message: "Error en el servidor" });
     }
 };
-module.exports = {validarFormularios}
+
+module.exports = { validarFormularios };
--- Back/index.js
+++ Back/index.js
@@ -1,23 +1,22 @@
-const express = require('express')
-const bodyparser = require('body-parser')
-const cors = require('cors')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const ruta = require('./Routes/formularioRoutes');
 const app = express();
 
-
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const corsOptions = {
     origin: 'http://localhost:3000',
     methods: ['POST'],
 };
-app.use(cors());
+app.use(cors(corsOptions));
 
+app.use('/', ruta);
 
+const puerto = process.env.PORT || 4000;
 
-
-const puerto =process.env.port || 4000;
-
-app.listen(puerto,()=>{
+app.listen(puerto, () => {
     console.log(`Escuchando en el puerto ${puerto}`);
-})
+});
